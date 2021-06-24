fe9254dad4d0e55b7a8aa40301bbbfc94779b900
the new exploiter <86060558+thenewexploiter@users.noreply.github.com>
2021-06-24 14:58:24 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+# cmd command1: pip | (is show your wersion of pip)
+# cmd command2: pip install pynput | (pack to auto clicker)
+
+
+__author__ = 'thenewexploiter'
+
+print("███████████████████████████████████████████████████████████████████")
+print("██▀▄─██▄─██─▄█─▄─▄─█─▄▄─███─▄▄▄─█▄─▄███▄─▄█─▄▄▄─█▄─█─▄█▄─▄▄─█▄─▄▄▀█")
+print("██─▀─███─██─████─███─██─███─███▀██─██▀██─██─███▀██─▄▀███─▄█▀██─▄─▄█")
+print("▀▄▄▀▄▄▀▀▄▄▄▄▀▀▀▄▄▄▀▀▄▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀")
+print(" ")
+print("The Start Stop Key: R")
+print("Exit Key: M")
+print("Made By: " + __author__)
+
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+delay = 0.001
+button = Button.left
+start_stop_key = KeyCode(char='r')
+exit_key = KeyCode(char='m')
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super().__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_running = True
+
+    def start_clicking(self):
+        self.running = True
+
+    def stop_clicking(self):
+        self.running = False
+
+    def exit(self):
+        self.stop_clicking()
+        self.program_running = False
+
+    def run(self):
+        while self.program_running:
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
+
+mouse = Controller()
+click_thread = ClickMouse(delay, button)
+click_thread.start()
+
+def on_press(key):
+    if key == start_stop_key:
+        if click_thread.running:
+            click_thread.stop_clicking()
+        else:
+            click_thread.start_clicking()
+    elif key == exit_key:
+        click_thread.exit()
+        listener.stop()
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
